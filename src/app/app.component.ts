012f3481bcb7f3fa8770a743358703e7cd473465
Tomek <tmszjaros@gmail.com>
2020-11-07 20:11:49 +0100

    feat(aio): add all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  public todos: string [] = [];
+  todoForm = new FormGroup({
+    todo: new FormControl('', Validators.required)
+  });
+
+  public onSubmit(): void {
+    if (this.todoForm.value.todo !== '') {
+      this.todos.push(this.todoForm.value.todo);
+      this.todoForm.setValue({
+        todo: " "
+      });
+    }
+  }
 }
